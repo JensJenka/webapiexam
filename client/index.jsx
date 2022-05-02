a6b6149a63fb3b5df067d0220229109160a425ca
JensJenka <jensjk@hotmail.com>
2022-05-02 18:39:09 +0200

    no da

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function FrontPage() {
