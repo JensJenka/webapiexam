2f544a5936ac9afa5c24d7c0fc63c7958f067907
JensJenka <jensjk@hotmail.com>
2022-05-02 16:19:28 +0200

    [dev:client] Bundling ...

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,32 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Heyheey megga</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Jenseste Database</h1>
+      <div>
+        <Link to={"/login"}>Login</Link>
+        <Link to={"/register"}>Register new user</Link>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return <h1>Hello my dear, this is login</h1>;
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
