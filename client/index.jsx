605a1427683c545ad15caa3b71f782c61267977a
JensJenka <jensjk@hotmail.com>
2022-05-02 20:22:24 +0200

    Working thus far - big leap will come:D

--- client/index.jsx
+++ client/index.jsx
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function FrontPage() {
+function LoginLinks() {
   return (
     <div>
-      <h1>Jenseste Database</h1>
       <div>
-        <div>
-          <Link to={"/login"}>Login</Link>
-        </div>
-        <div>
-          <Link to={"/register"}>Register new user</Link>
-        </div>
+        <Link to={"/login"}>Login</Link>
       </div>
+      <div>
+        <Link to={"/register"}>Register new user</Link>
+      </div>
+    </div>
+  );
+}
+
+function FrontPage() {
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState();
+  useEffect(async () => {
+    setLoading(true);
+    const res = await fetch("/api/login");
+    setUser(await res.json());
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <div>Loading content...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Jenseste Database hihi trololo </h1>
+      {user ? (
+        <div>
+          {user.fullName}({user.username})
+        </div>
+      ) : (
+        <LoginLinks />
+      )}
     </div>
   );
 }
